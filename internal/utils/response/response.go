647fa63c1353750347d644f5e230221f8cbe502d
mdiktushar <md.tushar1804@gmail.com>
2026-07-13 17:54:57 +0600

    add: structured error structure

--- internal/utils/response/response.go
+++ internal/utils/response/response.go
@@ -5,8 +5,25 @@ import (
 	"net/http"
 )
 
+type Response struct {
+	Status string
+	Error string
+}
+
+const (
+	StatusOK = "OK"
+	StatusError = "Error"
+)
+
 func WriteJson(w http.ResponseWriter, status int, data interface {}) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(data)
+}
+
+func GeneralError(err error) Response {
+	return Response{
+		Status: StatusError,
+		Error: err.Error(),
+	}
 }
