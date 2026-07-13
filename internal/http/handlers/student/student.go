c3f3f7a15da6c4619ab23d7469e68c9c75e9ab67
mdiktushar <md.tushar1804@gmail.com>
2026-07-13 19:40:30 +0600

    post: create successfull

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -3,15 +3,18 @@ package student
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/mdiktushar/REST-API-with-GoLang/internal/storage"
 	"github.com/mdiktushar/REST-API-with-GoLang/internal/types"
 	"github.com/mdiktushar/REST-API-with-GoLang/internal/utils/response"
 )
 
-func New() http.HandlerFunc {
+func New(storage storage.Storage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var student types.Student
 
@@ -34,7 +37,19 @@ func New() http.HandlerFunc {
 			return
 		}
 
-		response.WriteJson(w, http.StatusCreated, map[string]string{"success": "Ok"})
+		lastId, err := storage.CreateStudent(
+			student.Name,
+			student.Email,
+			student.Age,
+		)
+
+		if err != nil {
+			response.WriteJson(w, http.StatusInternalServerError, response.GeneralError(err))
+			return
+		}
+		slog.Info("user created successfully", slog.String("userId", fmt.Sprint(lastId)))
+
+		response.WriteJson(w, http.StatusCreated, map[string]int64{"id": lastId})
 
 	}
 }
